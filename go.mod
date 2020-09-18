b8de8386399b34a3cb7cc59562d088cf5d3d111a
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2020-09-19 03:08:44 +0530

    Pull docker image if does not exist

--- go.mod
+++ go.mod
@@ -9,9 +9,10 @@ require (
 	github.com/docker/docker v1.13.1
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.12
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
-	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/mattn/go-isatty"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 	"io"
@@ -24,7 +26,6 @@ import (
 const (
 	perpetualCommand = "sleep"
 	defaultTimeout   = "3600"
-	defaultUserID    = "1000"
 	ciWorkingDir     = "/ci"
 )
 
@@ -96,22 +97,25 @@ func runCogs(ctx context.Context, k cogsFile, client *docker.Client) error {
 func runTask(ctx context.Context, t task, client *docker.Client) error {
 	log.Println("Starting containers")
 
-	containerConfig := &container.Config{
-		User:       defaultUserID,
-		Image:      t.Image,
-		WorkingDir: ciWorkingDir,
-		Cmd:        []string{perpetualCommand, defaultTimeout},
-	}
-
 	cwd, err := os.Getwd()
 
 	if err != nil {
-		return errors.Wrap(err, "Cannot determine cwd")
+		return errors.Wrap(err, "cannot determine cwd")
+	}
+
+	err = pullImage(ctx, t, client, err)
+	if err != nil {
+		return errors.Wrap(err, "cannot pull docker image")
 	}
 
 	containerName := fmt.Sprintf("%s-%d", t.Name, time.Now().Unix())
+
 	createdContainer, err := client.ContainerCreate(ctx,
-		containerConfig,
+		&container.Config{
+			Image:      t.Image,
+			WorkingDir: ciWorkingDir,
+			Cmd:        []string{perpetualCommand, defaultTimeout},
+		},
 		&container.HostConfig{
 			Mounts: []mount.Mount{
 				{
@@ -131,7 +135,7 @@ func runTask(ctx context.Context, t task, client *docker.Client) error {
 	err = client.ContainerStart(ctx, createdContainer.ID, types.ContainerStartOptions{})
 
 	if err != nil {
-		return errors.Wrap(err, "Error starting container")
+		return errors.Wrap(err, "error starting container")
 	}
 
 	defer func() {
@@ -151,7 +155,7 @@ func runTask(ctx context.Context, t task, client *docker.Client) error {
 	exitCode, err := runScript(err, client, ctx, createdContainer, t.BeforeScript)
 
 	if err != nil {
-		return errors.Wrap(err, "Error executing before_script")
+		return errors.Wrap(err, "error executing before_script")
 	}
 
 	if exitCode != 0 {
@@ -162,14 +166,14 @@ func runTask(ctx context.Context, t task, client *docker.Client) error {
 	scriptExitCode, err := runScript(err, client, ctx, createdContainer, t.Script)
 
 	if err != nil {
-		return errors.Wrap(err, "Error executing script")
+		return errors.Wrap(err, "error executing script")
 	}
 
 	log.Println("Executing after_script")
 	exitCode, err = runScript(err, client, ctx, createdContainer, t.AfterScript)
 
 	if err != nil {
-		return errors.Wrap(err, "Error executing after_script")
+		return errors.Wrap(err, "error executing after_script")
 	}
 
 	if exitCode != 0 {
@@ -183,9 +187,24 @@ func runTask(ctx context.Context, t task, client *docker.Client) error {
 	return nil
 }
 
+func pullImage(ctx context.Context, t task, client *docker.Client, err error) error {
+	res, err := client.ImagePull(ctx, t.Image, types.ImagePullOptions{})
+
+	if err != nil {
+		return errors.Wrap(err, "cannot pull image from registry")
+	}
+
+	err = jsonmessage.DisplayJSONMessagesStream(res, os.Stdout, os.Stdout.Fd(), isatty.IsTerminal(os.Stdout.Fd()), nil)
+
+	if err != nil {
+		return errors.Wrap(err, "cannot display progress information for image pull")
+	}
+
+	return nil
+}
+
 func runScript(err error, client *docker.Client, ctx context.Context, createdContainer container.ContainerCreateCreatedBody, script []string) (int, error) {
 	execConfig := types.ExecConfig{
-		User:         "1000",
 		Privileged:   false,
 		Tty:          false,
 		AttachStdin:  true,
@@ -198,13 +217,13 @@ func runScript(err error, client *docker.Client, ctx context.Context, createdCon
 	execCreated, err := client.ContainerExecCreate(ctx, createdContainer.ID, execConfig)
 
 	if err != nil {
-		return -1, errors.Wrap(err, "Cannot execute command inside container")
+		return -1, errors.Wrap(err, "cannot execute command inside container")
 	}
 
 	execAttached, err := client.ContainerExecAttach(ctx, execCreated.ID, execConfig)
 
 	if err != nil {
-		return -1, errors.Wrap(err, "Cannot attach to IO of running command")
+		return -1, errors.Wrap(err, "cannot attach to IO of running command")
 	}
 
 	log.Println("Streaming logs")
@@ -215,7 +234,7 @@ func runScript(err error, client *docker.Client, ctx context.Context, createdCon
 		err = streamOutput(execAttached.Reader)
 
 		if err != nil {
-			done <- errors.Wrap(err, "Error reading output from container")
+			done <- errors.Wrap(err, "error reading output from container")
 		}
 		done <- nil
 	}()
@@ -224,14 +243,14 @@ func runScript(err error, client *docker.Client, ctx context.Context, createdCon
 		err = mustWrite(execAttached.Conn, cmd)
 
 		if err != nil {
-			return -1, errors.Wrap(err, "Error executing command")
+			return -1, errors.Wrap(err, "error executing command")
 		}
 	}
 
 	err = execAttached.CloseWrite()
 
 	if err != nil {
-		return -1, errors.Wrap(err, "Error closing IO")
+		return -1, errors.Wrap(err, "error closing IO")
 	}
 
 	<-done
@@ -239,7 +258,7 @@ func runScript(err error, client *docker.Client, ctx context.Context, createdCon
 	result, err := client.ContainerExecInspect(ctx, execCreated.ID)
 
 	if err != nil {
-		return -1, errors.Wrap(err, "Error inspecting command execution")
+		return -1, errors.Wrap(err, "error inspecting command execution")
 	}
 
 	return result.ExitCode, nil
@@ -249,7 +268,7 @@ func runScript(err error, client *docker.Client, ctx context.Context, createdCon
 func streamOutput(reader *bufio.Reader) error {
 	size, err := io.Copy(os.Stdout, reader)
 	if err != nil {
-		return errors.Wrap(err, "Error reading from stream")
+		return errors.Wrap(err, "error reading from stream")
 	}
 
 	log.Printf("Generated %d bytes of log data\n", size)
@@ -260,7 +279,7 @@ func mustWrite(conn net.Conn, str string) error {
 	_, err := conn.Write([]byte(str + "\n"))
 
 	if err != nil {
-		return errors.Wrap(err, "Error writing to stream")
+		return errors.Wrap(err, "error writing to stream")
 	}
 	return nil
 }
